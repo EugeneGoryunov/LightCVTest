23645b0d15a7749db9c722a8f4fc3178051e696f
Eugene_Goryunov <gorunov@moedelo.org>
2023-06-26 00:09:35 +0300

    Add authorization test

---
+++ AuthTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Transactions;
+using LightCV.BL.Exception;
+using LightCV.DAL.Models;
+using LightCVTest.Helpers;
+
+namespace LightCVTest;
+
+public class AuthTest : Helpers.BaseTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task BaseRegistrationTest()
+    {
+        using (TransactionScope scope = Helper.CreatTransactionScope())
+        {
+            string email = Guid.NewGuid().ToString() + "@test.com";
+
+            //create user
+            int userId = await authBl.CreatUser(new UserModel()
+            {
+                Email = email,
+                Password = "qwe1234"
+            });
+
+            Assert.Throws<AuthorizationException>(delegate
+            {
+                authBl.Authenticate("qweasd", "111", false).GetAwaiter().GetResult();
+            });
+            
+            Assert.Throws<AuthorizationException>(delegate
+            {
+                authBl.Authenticate(email, "111", false).GetAwaiter().GetResult();
+            });
+            
+            Assert.Throws<AuthorizationException>(delegate
+            {
+                authBl.Authenticate("qweasd", "qwe1234", false).GetAwaiter().GetResult();
+            });
+            
+            await authBl.Authenticate(email, "qwe1234", false);
+        }
+    }
+}
